5bd4e799c420711b740b92f460ab170e0f85c73f
Danko Caboski <caboskidanko@gmail.com>
2024-09-20 20:29:49 -0300

    Feat: NavBar

--- dataViz/src/components/Nav.vue
+++ dataViz/src/components/Nav.vue
@@ -5,7 +5,10 @@ export default {
 </script>
 
 <template>
-  <div class="navbar">
+  <nav>
+    <div class="PIWrapper">
+      <img class="ProfileImage" src="../assets/puppy.jpg" alt="Logo" />
+    </div>
     <div class="paginas">
       <!-- <router-link to="/dash">Dashboard</router-link> -->
       <router-link to="/">Dashboard</router-link>
@@ -16,35 +19,60 @@ export default {
       <!-- <router-link to="/analytics">Análises</router-link> -->
       <router-link to="/">Análises</router-link>
       <!-- <router-link to="/options">Opções</router-link> -->
+      <router-link to="/">Opções</router-link>
     </div>
-  </div>
+    <div class="sair">
+      <router-link to="/login">Sair</router-link>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
-.navbar {
+nav {
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  background: var(--vt-c-black);
-  color: var(--branco2-pro4tech);
+  background: var(--azul1-pro4tech);
   padding: 1rem;
   min-height: calc(100vh - 7vh);
-
   margin: 3vh;
   border-radius: var(--raio_border-default);
 }
 
+.PIWrapper {
+  width: 100%;
+  max-width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.ProfileImage {
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+  border-color: aliceblue;
+  border-width: 2px;
+}
+
 .paginas {
+  height: 100%;
+  margin: 15px;
   display: flex;
   flex-direction: column;
-  align-items: start;
+  align-items: flex-start;
 }
 
-.navbar-item {
-  margin-right: 1rem;
+.sair {
+  height: 100%;
+  margin: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.router-link-active {
-  font-weight: bold;
+.sair a {
+  color: red;
 }
 </style>
