d177c82fd87fd954b679bb291358d21507a728a2
Alec <alecrondel@hotmail.com>
2024-09-23 21:39:06 -0300

    :feat: adicionando e testando vue-data-ui

---
+++ dataViz/src/components/GraphTest.vue
@@ -0,0 +1,64 @@
+<template>
+  <div style="width:600px;">
+    <VueDataUi
+      component="VueUiVerticalBar"
+      :dataset="dataset"
+      :config="config"
+    />
+  </div>
+</template>
+
+
+<script setup>
+  import { ref } from "vue";
+  import { VueDataUi } from "vue-data-ui";
+  import "vue-data-ui/style.css";
+
+  const dataset = ref([
+    {
+        name: "Serie 1",
+        value: 100,
+        children: [
+        {
+            name: "serie 1 child 1",
+            value: 80
+        },
+        {
+            name: "serie 1 child 2",
+            value: 20
+        },
+        ]
+    },
+    {
+        name: "Serie 2",
+        value: 345,
+    },
+    {
+        name: "Serie 3",
+        value: 210,
+    },
+    {
+        name: "Serie 4",
+        value: 188,
+    },
+    {
+        name: "Serie 5",
+        value: 120,
+        children: [
+        {
+            name: "Serie 5 child 1",
+            value: 60,
+        },
+        {
+            name: "Serie 5 child 2",
+            value: 20,
+        },
+        {
+            name: "Serie 5 child 3",
+            value: 40,
+        },
+        ]
+    }
+]);
+  const config = ref({});
+</script>
--- dataViz/src/views/HomeView.vue
+++ dataViz/src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <script setup>
 import DashboardCard from '../components/DashboardCard.vue'
+import GraphTest from '../components/GraphTest.vue'
 </script>
 
 <template>
   <DashboardCard title="ttt" description="DDDD" />
+
+  <GraphTest></GraphTest>
 </template>
