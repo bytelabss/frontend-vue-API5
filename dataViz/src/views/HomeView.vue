24210fb002af30d9610d93b9d0c856699997f102
Alec <alecrondel@hotmail.com>
2024-09-26 22:39:57 -0300

    :feat: integrando gráfico na home view

--- dataViz/src/views/HomeView.vue
+++ dataViz/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import TempoMedioDeContratacaoPorVaga from '@/components/TempoMedioDeContratacaoPorVaga.vue';
 import QuantidadeContratacoesPorRh from '@/components/QuantidadeContratacoesPorRh.vue';
+import TempoMedioPorProcessoSeletivo from '@/components/TempoMedioPorProcessoSeletivo.vue';
 </script>
 
 <template>
@@ -10,58 +11,10 @@ import QuantidadeContratacoesPorRh from '@/components/QuantidadeContratacoesPorR
     <div class="wrapper">
     <QuantidadeContratacoesPorRh/>
     </div>
+    <div class="wrapper">
+      <TempoMedioPorProcessoSeletivo />
+    </div>
 </template>
-
-<script setup>
-  import DashboardCard from '../components/DashboardCard.vue'
-  import ResumeCard from '../components/ResumeCard.vue'
-  import GraphTest from '../components/GraphTest.vue'
-  import { onMounted, ref, computed } from 'vue';
-  import apiClient from '@/api/client';
-
-  // const chartData = ref([]);
-
-  // const plainChartData = computed(() => [...chartData.value]);
-
-  // const transformData = (data) => {
-
-  //   const transformedEntries = Object.entries(data).map(([key, value]) => {
-  //     return [key, value];
-  //   });
-
-  //   chartData.value = transformedEntries;
-
-  //   console.log("new chartdata: ", chartData.value);
-
-    
-  // };
-
-  // console.log("plain: ", plainChartData);
-
-  // const fetchDataTempoMedio = async () => {
-
-  //   console.log("function fired");
-
-  //   try {
-  //     const response = await fetch('http://localhost:9090/api/fatoContratacoes/tempo-medio-por-vaga');
-  //     if (!response.ok) {
-  //       throw new Error('Network response was not ok');
-  //     }
-  //     console.log("response: ", response);
-  //     const data = await response.json();
-  //     console.log("data: ", data );
-  //     transformData(data);
-  //   } catch (error) {
-  //     console.error("Erro ao pegar dados pela API: ", error)
-  //   }
-  // };
-
-  // onMounted(() => {
-  //   console.log("onmounted");
-  //   fetchDataTempoMedio();
-  // });
-
-</script>
 
 <style scoped>
 .wrapper {
